d01e544bf17c87a58aee3d297866e4b6de4d5489
nextor <nextor@gmail.com>
2019-11-11 12:14:26 -0800

    Sync changes

--- src/app/pages/gastos/gastos.component.ts
+++ src/app/pages/gastos/gastos.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { RestService } from '../../services/rest.service';
+import { Usuario,Gasto_Centro_Medico,Tipo_Gasto } from '../../models/Modelos';
+import { Router, ActivatedRoute } from "@angular/router"
+import { Location } from '@angular/common';
+import { forkJoin } from 'rxjs';
+import { BasePage } from '../../pages/base/base.page';
+import { SearchGastoCentroMedicoResponse } from '../../models/Respuestas';
 
 @Component({
-  selector: 'app-gastos',
-  templateUrl: './gastos.component.html',
-  styleUrls: ['./gastos.component.css']
+	selector: 'app-gastos',
+	templateUrl: './gastos.page.html',
+	styleUrls: ['./gastos.page.scss'],
 })
-export class GastosComponent implements OnInit {
+export class GastosComponent extends BasePage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	tipos_gasto:Tipo_Gasto[] = [];
+	gastos:SearchGastoCentroMedicoResponse[] = [];
 
+	ngOnInit() {
+		let usuario = this.rest.getUsuarioSesion();
+
+		forkJoin
+		(
+			[
+				//getTiposGastos({id_organizacion: usuario.id_organizacion}),
+				//this.rest.getGastos({ id_centro_medico: 1 })
+				this.rest.tipo_gasto.getAll({id_organizacion: usuario.id_organizacion}),
+				this.rest.searchGastoCentroMedico.getAll({ id_centro_medico: 1 }) //TODO FIX ponerlo de la session o seleccionarlo
+			]
+		).subscribe
+		(
+			(response:any[])=>
+			{
+				console.log("gastos",response);
+
+				this.tipos_gasto = response[0].datos;
+				this.gastos	= response[1].datos;//TODO Cambiar al usaurio de la sesion
+				this.is_loading = false;
+			}
+			,(error)=>
+			{
+				this.showError( error );
+			}
+		);
+	}
 }
