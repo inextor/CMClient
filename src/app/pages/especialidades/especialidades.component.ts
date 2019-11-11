98b04538d995e5245a311d554d66e88a58da7a9d
nextor <nextor@gmail.com>
2019-11-11 12:23:23 -0800

    Sync from old project

--- src/app/pages/especialidades/especialidades.component.ts
+++ src/app/pages/especialidades/especialidades.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { RestService } from '../../services/rest.service';
+import { Usuario,Servicio,Especialidad} from '../../models/Modelos';
+import {Router,ActivatedRoute} from "@angular/router"
+import { BasePage } from '../base/base.page';
 
 @Component({
   selector: 'app-especialidades',
   templateUrl: './especialidades.component.html',
   styleUrls: ['./especialidades.component.css']
 })
-export class EspecialidadesComponent implements OnInit {
+export class EspecialidadesComponent extends BasePage implements OnInit {
 
-  constructor() { }
+especialidades:Especialidad[]=[];
+	public statusmenu: boolean;
+	ngOnInit()
+	{
+		this.statusmenu = this.rest.statusMenu();
+		this.is_loading = true;
+		// this.rest.especialidad.getAll({}).subscribe((respuesta)=>
+		// this.rest.especialidad.getAll({}).subscribe((respuesta)=>
 
-  ngOnInit() {
-  }
+		this.route.paramMap.subscribe( params =>
+		{
+			this.currentPage = params.get('page') == null ? 0 : parseInt(params.get('page') );
 
+			this.rest.especialidad.getAll({},{page:this.currentPage, page_size: 10}).subscribe((respuesta) =>
+			{
+				this.especialidades = respuesta.datos;
+				console.log(this.especialidades)
+				
+				this.setPages( this.currentPage, respuesta.total );
+			},(error)=>
+			{
+				this.showError(error);
+			});
+		});
+	}
+	activeMenu(){
+		this.rest.activeMenu();
+		this.statusmenu = this.rest.statusMenu();
+	}
 }
