d06a9c2c2dfc70f03bccb398302d4b61aef5007c
nextor <nextor@gmail.com>
2019-11-11 12:34:32 -0800

    Sync from old project

--- src/app/pages/centros-medicos/centros-medicos.component.ts
+++ src/app/pages/centros-medicos/centros-medicos.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import	{	Component,	OnInit	}	from	'@angular/core';
+import	{	AlertController	}	from	'@ionic/angular';
+import	{	RestService	}	from	'../../services/rest.service';
+import	{	Usuario,Doctor,Centro_Medico	}	from	'../../models/Modelos';
+import	{Router,ActivatedRoute}	from	"@angular/router"
+
+
 
 @Component({
   selector: 'app-centros-medicos',
@@ -7,9 +13,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CentrosMedicosComponent implements OnInit {
 
-  constructor() { }
+	constructor(private	rest:RestService,public	alertController:	AlertController,private	router:Router,private	route:ActivatedRoute)	{	}
 
-  ngOnInit() {
-  }
+	is_loading:boolean 	= false;
+	centros_medicos:Centro_Medico[]		= [];
 
+	ngOnInit()	{
+		// TODO agarrar id organizacion de la sesion
+
+		//this.rest.getCentrosMedicosPorOrganizacion(1).subscribe((respuesta)=>
+		this.rest.centro_medico.getAll({ id_organizacion: 1 }).subscribe((respuesta)=>
+		{
+			this.centros_medicos = respuesta.datos;
+		});
+	}
 }
