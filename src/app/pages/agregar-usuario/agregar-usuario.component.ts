ccdb0f37cb7e7ce87b42f5d0781c4db39a727e88
nextor <nextor@gmail.com>
2019-11-11 12:33:49 -0800

    Sync from old project

--- src/app/pages/agregar-usuario/agregar-usuario.component.ts
+++ src/app/pages/agregar-usuario/agregar-usuario.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { RestService } from '../../services/rest.service';
+import { Usuario , Paciente } from '../../models/Modelos';
+import {Router,ActivatedRoute} from "@angular/router"
+
+
 
 @Component({
   selector: 'app-agregar-usuario',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarUsuarioComponent implements OnInit {
 
-  constructor() { }
+	constructor(private rest:RestService,public alertController: AlertController,private router:Router,private route:ActivatedRoute) { }
 
-  ngOnInit() {
-  }
+	usuario:Usuario = {
+		id_organizacion: 1,
+		contrasena: '',
+		correo_electronico:'',
+		tipo: 'PACIENTE'
+	};
+	paciente:Paciente = {
+		nombre:'',
+		apellidos:''
+	};
+	is_loading:boolean = false;
 
+	confirmar_contrasena:string = '';
+
+	ngOnInit()
+	{
+
+	}
+
+	async showError(message:string)
+	{
+		const alert = await this.alertController.create
+		({
+			header: 'Error',
+			//subHeader: 'Subtitle',
+			message: message,
+			buttons: ['OK']
+		});
+
+		await alert.present();
+	}
+
+	registrarse()
+	{
+	
+		this.is_loading = true;
+		this.rest.registrarUsuarioPaciente( this.usuario, this.paciente ).subscribe((usuario)=>
+		{
+			this.is_loading = false;
+			this.router.navigate(['/home']);
+		},
+		(error)=>
+		{
+			console.log("QUE PASO");
+			 this.showError( this.rest.getErrorMessage( error ) );
+			this.is_loading = false;
+		});
+	}
 }
