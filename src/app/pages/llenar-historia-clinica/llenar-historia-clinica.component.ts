018bfa8384bd64266f8eebd5ee974f27335b51f3
nextor <nextor@gmail.com>
2019-11-11 12:13:21 -0800

    Sync changes

--- src/app/pages/llenar-historia-clinica/llenar-historia-clinica.component.ts
+++ src/app/pages/llenar-historia-clinica/llenar-historia-clinica.component.ts
@@ -1,15 +1,106 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+import { RestService } from '../../services/rest.service';
+import { Usuario, Doctor,Paciente } from '../../models/Modelos';
+import { Router,ActivatedRoute } from "@angular/router"
+import { SeleccionarCentroMedicoComponent } from '../../components/seleccionar-centro-medico/seleccionar-centro-medico.component';
+import { PreguntasHistoriaClinicaResponse } from '../../models/Respuestas';
+import { switchMap,catchError } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+import { BasePage } from '../base/base.page';
+import { RespuestaPreguntaHistoriaClinica } from '../../models/Respuestas';
+import { Respuesta } from '../../models/Respuestas';
+import { Respuesta_Historia_Clinica } from '../../models/Modelos';
 
 @Component({
   selector: 'app-llenar-historia-clinica',
   templateUrl: './llenar-historia-clinica.component.html',
   styleUrls: ['./llenar-historia-clinica.component.css']
 })
-export class LlenarHistoriaClinicaComponent implements OnInit {
+export class LlenarHistoriaClinicaComponent  extends BasePage implements OnInit {
 
-  constructor() { }
+	preguntas:RespuestaPreguntaHistoriaClinica[] = [];
+	doctor:Doctor = {};
+	usuario:Usuario = null;
+	paciente:Paciente = {};
+	msgError:string = null;
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.is_loading = true;
+		this.route.paramMap.pipe(
+			switchMap(params=>
+			{
+				let idDoctor	= parseInt( params.get('idDoctor') );
+				let idPaciente	= parseInt( params.get('idPaciente') );
 
+				return forkJoin([
+					this.rest.doctor.get( idDoctor )
+					,this.rest.paciente.get(idPaciente)
+				])
+
+			}),
+			switchMap(response=>
+			{
+				console.log("doctor y paciente",response );
+				this.doctor = response[0];
+				this.paciente = response[1];
+
+				//If id_especialidad is null then 1
+				//
+
+				return this.rest.getPreguntasRespuestasHistoriaClinica(  this.paciente.id,this.doctor.id, this.doctor.id_especialidad );
+			}),
+			catchError(error=>
+			{
+				this.is_loading = false;
+				this.msgError = this.rest.getErrorMessage( error );
+				return null;
+			})
+		).subscribe((response:Respuesta<RespuestaPreguntaHistoriaClinica>)=>
+		{
+			console.log('Preguntas',response);
+
+			response.datos.forEach((i)=>
+			{
+				if( i.respuesta == null )
+				{
+					i.respuesta = {
+						respuesta_binaria: null,
+						respuesta: null,
+						id_pregunta_historia_clinica : i.pregunta.id,
+						id_paciente: this.paciente.id,
+						id_doctor: this.doctor.id
+					};
+					console.log("NOT NULL NOW");
+				}
+			});
+
+			console.log( response.datos );
+			this.preguntas = response.datos;
+			this.is_loading = false;
+
+		},(error)=>
+		{
+			this.showError( error );
+			//this.msgError = this.rest.getErrorMessage( error );
+			//console.log( this.msgError );
+		});
+	}
+
+	guardar()
+	{
+		this.is_loading = true;
+		let respuestas:Respuesta_Historia_Clinica[] = this.preguntas.map( i => i.respuesta );
+
+		this.rest.guardarRespuestasHistoriaClinica( respuestas ).subscribe((respuestas)=>
+		{
+			this.is_loading = false;
+		}
+		,(error)=>
+		{
+			this.showError( error );
+			this.is_loading = false;
+		});
+	}
 }
--- src/app/pages/mis-citas/mis-citas.component.ts
+++ src/app/pages/mis-citas/mis-citas.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { RestService } from '../../services/rest.service';
+import { Usuario,Doctor,Paciente } from '../../models/Modelos';
+import { SearchCitaResponse } from '../../models/Respuestas';
+import {Router,ActivatedRoute} from "@angular/router"
+import { Cita } from '../../models/Modelos';
 @Component({
   selector: 'app-mis-citas',
   templateUrl: './mis-citas.component.html',
@@ -7,9 +12,237 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisCitasComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+		private rest:RestService,
+		public alertController:AlertController,
+		private router:Router,
+		private route:ActivatedRoute,
+	) { }
 
-  ngOnInit() {
-  }
+	cita:Cita = {};
+	info_citas:SearchCitaResponse[] = [];
+	orderBy = 'Fecha';
+	orderDirection='ASC';
+
+	ngOnInit() {
+		let d = new Date();
+		d.setHours( d.getHours() - 3 );
+		let z = (i)=> i<10 ? '0'+i : i;
+
+		//this.rest.searchCitas({fecha_inicio: d.getFullYear()+'-'+z(d.getMonth()+1)+'-'+z(d.getDate())+' '+z(d.getHours())+':'+z(d.getMinutes())+':00'}).subscribe((respuesta)=>
+		this.rest.searchCita.getAll({fecha_inicio: d.getFullYear()+'-'+z(d.getMonth()+1)+'-'+z(d.getDate())+' '+z(d.getHours())+':'+z(d.getMinutes())+':00'}).subscribe((respuesta)=>
+		{
+			console.log( respuesta.datos );
+			this.info_citas = respuesta.datos;
+		});
+	}
+
+	changeSearch( evt )
+	{
+		console.log("FOOOOO make a search", evt);
+		this.search( evt.target.value );
+	}
+
+	ordenar(item)
+	{
+		if( this.orderBy === item )
+		{
+			this.orderDirection == 'ASC' ? 'DESC' : 'ASC';
+		}
+		else
+		{
+			this.orderBy = item;
+			this.orderDirection = 'ASC';
+		}
+	}
+
+	search(nombre)
+	{
+		//this.rest.searchCitas({ nombre }).subscribe((respuesta)=>
+		this.rest.searchCita.getAll({ nombre }).subscribe((respuesta)=>
+		{
+			this.info_citas = respuesta.datos;
+		});
+	}
+
+	confirmarDoctor(id_cita:number)
+	{
+		this.alertController.create({
+			header: 'Alerta',
+			subHeader: 'Confirmar doctor',
+			message: 'El doctor ha confirmado la cita?',
+			buttons: [
+				{
+					text: 'Cancelar',
+					role:'cancel',
+					handler:(result)=>
+					{
+						console.log("Cancelar");
+					}
+				},
+				{
+					text: 'Ok',
+					handler: (result)=>
+					{
+						//this.rest.updateCita({
+						this.rest.cita.update({
+							id: id_cita
+							,confirmado_por_doctor: 'SI'
+						}).subscribe((cita)=>
+						{
+							let index = this.info_citas.findIndex(i=> i.cita.id ==  id_cita );
+							if( index >= 0 )
+								this.info_citas[ index ].cita = cita;
+						});
+						console.log("OK");
+					}
+				}
+			]
+		}).then((alert)=>
+		{
+			alert.present().then((result)=>
+			{
+				console.log( result );
+			},
+			(error)=>{
+				console.log('Ocurrio un error',error);
+			});
+		});
+	}
+
+	confirmarPaciente(id_cita:number)
+	{
+		let alert = this.alertController.create({
+			header: 'Alerta',
+			subHeader: 'Confirmar paciente',
+			message: 'El paciente ha confirmado la cita?',
+			buttons: [
+				{
+					text: 'Cancelar',
+					role:'cancel',
+					handler:(result)=>
+					{
+						console.log("Cancelar");
+					}
+				},
+				{
+					text: 'Ok',
+					handler: (result)=>
+					{
+						console.log("OK");
+						//this.rest.updateCita({
+						this.rest.cita.update({
+							id: id_cita
+							,confirmado_por_paciente : 'SI'
+						}).subscribe((cita)=>
+						{
+							let index = this.info_citas.findIndex(i=> i.cita.id ==  id_cita );
+							if( index >= 0 )
+								this.info_citas[ index ].cita = cita;
+						});
+
+					}
+				}
+			]
+		}).then((alert)=>
+		{
+			alert.present().catch((error)=>{
+				console.log('Ocurrio un error',error);
+			});
+		});
+	}
+
+	cancelar(id_cita:number)
+	{
+		this.alertController.create({
+			header: 'Alerta',
+			subHeader: 'Cancelar la cita',
+			message: 'Esta seguro que desea cancelar la cita?',
+			buttons: [
+				{
+					text: 'No',
+					role:'cancel',
+					handler:(result)=>
+					{
+						console.log("Cancelar");
+					}
+				},
+				{
+					text: 'Si',
+					handler: (result)=>
+					{
+						//this.rest.updateCita({
+						this.rest.cita.update({
+							id: id_cita
+							,estatus: 'CANCELADA'
+						}).subscribe((cita)=>
+						{
+							console.log( id_cita );
+							let index = this.info_citas.findIndex(i=> i.cita.id ==  id_cita );
+							if( index >= 0 )
+								this.info_citas[ index ].cita = cita;
+						});
+
+					}
+				}
+			]
+		}).then((alert)=>
+		{
+			alert.present().then((result)=>
+			{
+				console.log( result );
+			},
+			(error)=>{
+				console.log('Ocurrio un error',error);
+			});
+		});
+	}
+
+	activar(id_cita:number)
+	{
+		this.alertController.create({
+			header: 'Alerta',
+			subHeader: 'Activar la cita',
+			message: 'Esta seguro que desea activar la cita?',
+			buttons: [
+				{
+					text: 'Cancelar',
+					role:'cancel',
+					handler:(result)=>
+					{
+						console.log("Cancelar");
+					}
+				},
+				{
+					text: 'Ok',
+					handler: (result)=>
+					{
+						console.log("OK");
+
+						//this.rest.updateCita({
+						this.rest.cita.update({
+							id: id_cita
+							,estatus: 'ACTIVA'
+						}).subscribe((cita)=>
+						{
+							let index = this.info_citas.findIndex(i=> i.cita.id ==  id_cita );
+							if( index >= 0 )
+								this.info_citas[ index ].cita = cita;
+						});
+					}
+				}
+			]
+
+		}).then((alert)=>
+		{
+			alert.present().then((result)=>
+			{
+				console.log( result );
+			},
+			(error)=>{
+				console.log('Ocurrio un error',error);
+			});
+		});
+	}
 
 }
