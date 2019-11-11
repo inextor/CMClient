7ef5899324cd9d91b8f2c101ab5ad1c5e09904fe
nextor <nextor@gmail.com>
2019-11-11 12:10:31 -0800

    Actualizando

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	AfterContentInit,
+	AfterViewInit,
+	OnDestroy
+} from '@angular/core';
+import { RestService } from '../../services/rest.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements implements OnInit, AfterContentInit, AfterViewInit, OnDestroy {
+	constructor(
+		private rest: RestService,
+		public alertController: AlertController,
+		private router: Router,
+		private route: ActivatedRoute,
+		public menuCtrl: MenuController
+	) {
+		if(this.rest.currentUserValue){
+			this.router.navigate(['/'])
+		}
+	}
 
-  constructor() { }
+	ngOnInit() {
+		this.menuCtrl.enable(false);
+		this.menuCtrl.swipeEnable(false);
+	}
+	ngAfterContentInit() {
+		this.menuCtrl.enable(true);
+		this.menuCtrl.swipeEnable(true);
+	}
+	ngAfterViewInit() {
+		this.menuCtrl.enable(false);
+		this.menuCtrl.swipeEnable(false);
+	}
+	ngOnDestroy() {
+		this.menuCtrl.enable(true);
+		this.menuCtrl.swipeEnable(true);
+	}
 
-  ngOnInit() {
-  }
+	usuario: string = '';
+	contrasena: string = '';
+	is_loading: boolean = false;
 
+	async showError(message: string) {
+		const alert = await this.alertController.create({
+			header: 'Error',
+			//subHeader: 'Subtitle',
+			message: message,
+			buttons: ['OK']
+		});
+
+		await alert.present();
+	}
+
+	doLoginKeyboard(evt:Event)
+	{
+		this.doLogin();
+	}
+
+	doLogin() {
+		this.is_loading = true;
+
+		this.rest.doLogin(this.usuario, this.contrasena).subscribe(
+			response => {
+				this.is_loading = false;
+				this.router.navigate(['/home']);
+				//this.restService.callMethodGet('/assets/data.json',{foo:'yes'}).subscribe((response)=>
+				console.log(response);
+			},
+			error => {
+				this.showError(this.rest.getErrorMessage(error));
+				this.is_loading = false;
+			}
+		);
+	}
 }
