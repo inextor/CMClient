f3d110b3fd3beeec48fe1cee9adf281b82a600b9
nextor <nextor@gmail.com>
2019-11-11 12:33:38 -0800

    Sync from old project

--- src/app/pages/agregar-tipo-precio/agregar-tipo-precio.component.ts
+++ src/app/pages/agregar-tipo-precio/agregar-tipo-precio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { RestService } from '../../services/rest.service';
+import {Router,ActivatedRoute} from "@angular/router";
+import { Tipo_Precio, Usuario } from '../../models/Modelos';
 
 @Component({
   selector: 'app-agregar-tipo-precio',
@@ -6,10 +10,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-tipo-precio.component.css']
 })
 export class AgregarTipoPrecioComponent implements OnInit {
-
-  constructor() { }
+  is_loading:boolean	= false;
+  currentUser:Usuario;
+  tipoPrecio:Tipo_Precio = {
+		nombre: '',
+		id_organizacion: null,
+  };
+  
+  constructor(private rest:RestService,public alertController: AlertController,private router:Router,private route:ActivatedRoute) { }
 
   ngOnInit() {
+	this.currentUser = this.rest.getUsuarioSesion();
+    this.tipoPrecio = {
+			id: null,
+			nombre:'',
+			id_organizacion:this.currentUser.id_organizacion,
+		};
+
+		this.route.paramMap.subscribe( params =>
+		{
+			let id = params.get('id') ==null ? null : parseInt(params.get('id') );
+			if( id != null )
+			{
+				this.is_loading = true;
+				//this.rest.getCentroMedico( id ).subscribe((centro_medico)=>
+				this.rest.tipo_precio.get( id ).subscribe((tipoPrecio)=>
+				{
+					this.is_loading = false;
+					this.tipoPrecio = tipoPrecio;
+				});
+			}
+		});
   }
+  agregar()
+	{
+		this.is_loading = true;
+
+		if( this.tipoPrecio.id	)
+		{
+			//this.rest.actualizarCentroMedico( this.centro_medico ).subscribe((centro_medico)=>{
+      			this.rest.tipo_precio.update( this.tipoPrecio ).subscribe((tipoPrecio)=>{
+				this.is_loading = false;
+				this.router.navigate(['/tipo-precios/0']);
+			});
+		}
+		else
+		{
+			//this.rest.agregarCentroMedico( this.centro_medico ).subscribe((centro_medico)=>{
+				this.rest.tipo_precio.create( this.tipoPrecio ).subscribe((tipoPrecio)=>{
+				this.is_loading = false;
+				this.router.navigate(['/tipo-precios/0']);
+			});
+		}
+	}
 
 }
