cec9f8a53c2cfd3cfcd4bae0938b263f9018b913
Dhantox <dhantox2@hotmail.com>
2019-11-12 11:46:12 -0800

    cambios header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../../pages/base/base.component'
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent extends BaseComponent implements OnInit {
 
   ngOnInit() {
+      let usuario = this.rest.getUsuarioSesion();
   }
-
 }
