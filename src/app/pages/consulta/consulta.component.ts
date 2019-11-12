7a8f201d43ddf66d18204f140eb6cdf859834731
nextor <nextor@gmail.com>
2019-11-11 16:10:23 -0800

    Revolviendo gastos

--- src/app/pages/consulta/consulta.component.ts
+++ src/app/pages/consulta/consulta.component.ts
@@ -3,28 +3,30 @@ import {RestService} from '../../services/rest.service'
 import { Doctor,Cita, Usuario,Paciente,Centro_Medico} from '../../models/Modelos';
 import {Router,ActivatedRoute,ParamMap} from "@angular/router"
 import { Route } from '@angular/compiler/src/core';
+import { BaseComponent } from '../base/base.component';
 
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.component.html',
   styleUrls: ['./consulta.component.css']
 })
-export class ConsultaComponent implements OnInit {
+export class ConsultaComponent extends BaseComponent implements OnInit {
 
-  constructor(private rest :RestService, private route: Route, private paramMap:ParamMap) { }
+	paciente:Paciente = {};
+	doctor:Doctor = {};
 
   ngOnInit() {
     let usuario = this.rest.getUsuarioSesion();
     this.rest.doctor.get( usuario.id ).subscribe(doctor=> this.doctor = doctor);
     this.route.paramMap.subscribe( params =>{
       let idPaciente = params.get('id_paciente') == null ? null : parseInt( params.get('id_paciente') );
-    
+
       if(idPaciente){
         this.rest.paciente.get(idPaciente).subscribe(paciente => {this.paciente = paciente});
       }
-    
+
     });
-  
+
   }
 
 }
