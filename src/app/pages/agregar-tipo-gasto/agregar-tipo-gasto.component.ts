161d5e29fef0f501cb735694871d19f17f4ee0b6
nextor <nextor@gmail.com>
2019-11-13 12:12:21 -0800

    Borrando directorios que no se usan

--- src/app/pages/agregar-tipo-gasto/agregar-tipo-gasto.component.ts
+++ src/app/pages/agregar-tipo-gasto/agregar-tipo-gasto.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario,Tipo_Gasto } from '../../models/Modelos';
+import { BaseComponent } from '../base/base.component';
+
+
 
 @Component({
-  selector: 'app-agregar-tipo-gasto',
-  templateUrl: './agregar-tipo-gasto.component.html',
-  styleUrls: ['./agregar-tipo-gasto.component.css']
+	selector: 'app-agregar-tipo-gasto',
+	  templateUrl: './agregar-tipo-gasto.component.html',
+	styleUrls: ['./agregar-tipo-gasto.component.css']
 })
-export class AgregarTipoGastoComponent implements OnInit {
+export class AgregarTipoGastoComponent  extends BaseComponent implements OnInit {
 
-  constructor() { }
+	id_tipo_gasto = null;
+	tipoGasto:Tipo_Gasto={
 
-  ngOnInit() {
-  }
+	};
 
+	ngOnInit() {
+		this.route.paramMap.subscribe( params =>
+		{
+			this.id_tipo_gasto = params.get('id') == null ? null : parseInt( params.get('id') );
+			if( this.id_tipo_gasto )
+			{
+				//this.rest.getTipoGasto(this.id_tipo_gasto).subscribe((tipoGasto)=>
+				this.rest.tipo_gasto.get(this.id_tipo_gasto).subscribe((tipoGasto)=>
+				{
+					this.tipoGasto =  tipoGasto;
+				}
+				,(error)=>
+				{
+					let msg = this.rest.getErrorMessage( error );
+					console.log( msg );
+				});
+			}
+		});
+	}
+
+	guardar()
+	{
+		console.log("Guardando");
+		if( this.id_tipo_gasto )
+		{
+			//this.rest.updateTipoGasto( this.tipoGasto ).subscribe(()=>
+			this.rest.tipo_gasto.update( this.tipoGasto ).subscribe((tipo_gasto)=>
+			{
+
+			},(error)=>
+			{
+				this.showError( error );
+			});
+		}
+		else
+		{
+			//this.rest.agregarTipoGasto( this.tipoGasto ).subscribe((tipo_gasto)=>
+			this.rest.tipo_gasto.create( this.tipoGasto ).subscribe((tipo_gasto)=>
+			{
+				this.location.back();
+			});
+		}
+	}
 }
--- src/app/pages/proveedores/proveedores.component.ts
+++ src/app/pages/proveedores/proveedores.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import	{	Component,	OnInit	}	from	'@angular/core';
+import	{	AlertController	}	from	'@ionic/angular';
+import	{	RestService	}	from	'../../services/rest.service';
+import	{	Usuario,Doctor,Centro_Medico, Proveedor	}	from	'../../models/Modelos';
+import	{Router,ActivatedRoute}	from	"@angular/router"
+import { BaseComponent } from '../base/base.component';
 
 @Component({
   selector: 'app-proveedores',
   templateUrl: './proveedores.component.html',
   styleUrls: ['./proveedores.component.css']
 })
-export class ProveedoresComponent implements OnInit {
+export class ProveedoresComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
-
+	proveedores: Proveedor[]		= [];
   ngOnInit() {
+    this.is_loading = true;
+    this.rest.proveedor.getAll({ id_organizacion: 1 }).subscribe((respuesta)=>
+		{
+	this.is_loading = false;
+      this.proveedores = respuesta.datos;
+      console.log(this.proveedores)
+		});
   }
 
 }
