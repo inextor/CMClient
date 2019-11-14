1471edb80ff0f95288490536a941f751b4e678d8
Dhantox <dhantox2@hotmail.com>
2019-11-14 09:24:27 -0800

    cambios app laonding en cada page

--- src/app/pages/agregar-organizacion/agregar-organizacion.component.ts
+++ src/app/pages/agregar-organizacion/agregar-organizacion.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import {BaseComponent} from '../base/base.component'
 @Component({
   selector: 'app-agregar-organizacion',
   templateUrl: './agregar-organizacion.component.html',
   styleUrls: ['./agregar-organizacion.component.css']
 })
-export class AgregarOrganizacionComponent implements OnInit {
-
-  constructor() { }
+export class AgregarOrganizacionComponent extends BaseComponent implements OnInit {
 
   ngOnInit() {
   }
