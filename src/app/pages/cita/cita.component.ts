618f1a0dfc6b635226a30b6fb3694f5ebb806f01
nextor <nextor@gmail.com>
2019-11-19 16:09:51 -0800

    Agregando estilos

--- src/app/pages/cita/cita.component.ts
+++ src/app/pages/cita/cita.component.ts
@@ -5,50 +5,50 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { CitasService } from 'src/app/services/citas.service';
 
 @Component({
-  selector: 'app-cita',
-  templateUrl: './cita.component.html',
-  styleUrls: ['./cita.component.css']
+	selector: 'app-cita',
+	templateUrl: './cita.component.html',
+	styleUrls: ['./cita.component.css']
 })
 export class CitaComponent implements OnInit {
 
-  constructor(private citasService:CitasService) { }
+	constructor(private citasService:CitasService) { }
 // TODO
-  ngOnInit() {
-    const events = this.citasService.getDisponibilidadDoctor(2, 2).subscribe(citas => {
-      this.calendarEvents = citas.datos.map(event => {
-        const id = this.counterId;
-        this.counterId += 1;
-        return {
-          id: id,
-          tipo: "disponibilidad",
-          rendering: 'background',
+	ngOnInit() {
+		const events = this.citasService.getDisponibilidadDoctor(2, 2).subscribe(citas => {
+			this.calendarEvents = citas.datos.map(event => {
+				const id = this.counterId;
+				this.counterId += 1;
+				return {
+					id: id,
+					tipo: "disponibilidad",
+					rendering: 'background',
 			startTime: event.hora_inicio,//startTime,
 			endTime: event.hora_final, //endTime,
 			daysOfWeek: event.dia_semana,//event.daysOfWeek
-        }
-      });
-    })
-  }
+				}
+			});
+		})
+	}
 
-  calendarPlugins = [dayGridPlugin, TimeGrid, interactionPlugin];
-  counterId = 1;
-  calendarEvents = [];
+	calendarPlugins = [dayGridPlugin, TimeGrid, interactionPlugin];
+	counterId = 1;
+	calendarEvents = [];
 
-  addEvent(event){
+	addEvent(event){
 		this.calendarEvents.push({
-      id: this.counterId,
-      title: 'Disponible',
-      start: event.start,
-      end: event.end
-    });
+			id: this.counterId,
+			title: 'Disponible',
+			start: event.start,
+			end: event.end
+		});
 	}
 
 	deleteEvent(events, eventId){
 		const calendarEvents = events.slice();
-    const event = calendarEvents.find((event) => event.id == eventId)
-    if(event.rendering === 'background'){
-      return calendarEvents;
-    }
+		const event = calendarEvents.find((event) => event.id == eventId)
+		if(event.rendering === 'background'){
+			return calendarEvents;
+		}
 		const index = calendarEvents.indexOf(event);
 		calendarEvents.splice(index, 1);
 		return calendarEvents;
@@ -58,7 +58,7 @@ export class CitaComponent implements OnInit {
 		const calendarEvents = this.deleteEvent(events, eventId);
 		calendarEvents.push({id: newEvent.id, title: 'Disponible', start: newEvent.start, end: newEvent.end})
 		return calendarEvents;
-  }
+	}
 
 	eventClick(info) {
 		this.calendarEvents = this.deleteEvent(this.calendarEvents, info.event.id);
@@ -73,10 +73,10 @@ export class CitaComponent implements OnInit {
 	}
 
 	dateClick(date) {
-    const createdEvent = this.calendarEvents.find(event => event.tipo === undefined);
-    if(createdEvent){
-      this.calendarEvents = this.deleteEvent(this.calendarEvents, createdEvent.id);
-    }
+		const createdEvent = this.calendarEvents.find(event => event.tipo === undefined);
+		if(createdEvent){
+			this.calendarEvents = this.deleteEvent(this.calendarEvents, createdEvent.id);
+		}
 		let calendarEvents = this.calendarEvents.slice();
 		calendarEvents.push({ id: this.counterId, title: 'Disponible', start: date.date, end: date.date });
 		this.calendarEvents = calendarEvents;
--- src/app/pages/consulta/consulta.component.ts
+++ src/app/pages/consulta/consulta.component.ts
@@ -5,34 +5,86 @@ import {Router,ActivatedRoute,ParamMap} from "@angular/router"
 import { Route } from '@angular/compiler/src/core';
 import { BaseComponent } from '../base/base.component';
 import { Location } from	'@angular/common';
+import { forkJoin } from 'rxjs'
 
 
 @Component({
-  selector: 'app-consulta',
-  templateUrl: './consulta.component.html',
-  styleUrls: ['./consulta.component.css']
+	selector: 'app-consulta',
+	templateUrl: './consulta.component.html',
+	styleUrls: ['./consulta.component.css']
 })
 export class ConsultaComponent extends BaseComponent implements OnInit {
 
 	paciente:Paciente = {};
-  doctor:Doctor = {};
-  
-  constructor(public rest:RestService,public router:Router,public route:ActivatedRoute,public location: Location) {
-	  super( rest,router,route,location);
+	doctor:Doctor = {};
+	consulta:Consulta = {};
+
+	constructor(public rest:RestService,public router:Router,public route:ActivatedRoute,public location: Location)
+	{
+		super( rest,router,route,location);
 	}
 
-  ngOnInit() {
-    let usuario = this.rest.getUsuarioSesion();
-    this.rest.doctor.get( usuario.id ).subscribe(doctor=> this.doctor = doctor);
-    this.route.paramMap.subscribe( params =>{
-      let idPaciente = params.get('id_paciente') == null ? null : parseInt( params.get('id_paciente') );
+	ngOnInit()
+	{
+		this.route.paramMap.subscribe( params =>
+		{
+			let id	= params.get('id') ? parseInt( params.get('id') ) : null;
 
-      if(idPaciente){
-        this.rest.paciente.get(idPaciente).subscribe(paciente => {this.paciente = paciente});
-      }
+			if( id )
+			{
+				this.consulta ={
+					motivo		: '',
+					diagnostico	: '',
+					tratamiento	: '',
+					id_paciente	: id_paciente,
+					id_doctor	: id_doctor,
+				};
 
-    });
+				this.is_loading = true;
+				this.rest.consulta.get( id ).subscribe((consulta)=>
+				{
+					this.consulta = consulta;
+					forkJoin([this.rest.paciente.get( consulta.id_paciente ),this.rest.doctor.get( consulta.id_doctor )])
+					.subscribe(results=>
+					{
+						this.paciente = results[0];
+						this.doctor = results[1];
+					},error=>this.showError(error));
+				},error=>this.showError(error));
+			}
+			else
+			{
+				let usuario = this.rest.getUsuarioSesion();
+				let id_paciente =  parseInt( params.get('id_paciente'));
 
-  }
+				forkJoin([this.rest.paciente.get( id_paciente ),this.rest.doctor.get( usuario.id )])
+				.subscribe(results=>
+				{
+					this.paciente = results[0];
+					this.doctor = results[1];
+				},error=>this.showError(error));
 
+
+				this.consulta ={
+					motivo		: '',
+					diagnostico	: '',
+					tratamiento	: '',
+					id_paciente	: id_paciente,
+					id_doctor	: id_doctor,
+				};
+			}
+		});
+	}
+
+	guardar()
+	{
+		this.is_loading	= true;
+		let observable	= this.consulta.id ? this.consulta.update( this.consulta ) : this.consulta.post( this.consulta );
+
+		observable.subscribe
+		(
+			consulta	=> this.consulta = consulta
+			,error 		=> this.showError( error )
+		);
+	}
 }
