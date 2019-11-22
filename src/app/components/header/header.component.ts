fe2fe8d80bb7f62c0ede46985c0b5603903f12d5
Dhantox <dhantox2@hotmail.com>
2019-11-22 10:31:46 -0800

    cambios header menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BaseComponent } from '../../pages/base/base.component'
-
+import { RestService } from 'src/app/services/rest.service';
+import { Router } from '@angular/router';
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
@@ -8,9 +9,19 @@ import { BaseComponent } from '../../pages/base/base.component'
 })
 export class HeaderComponent extends BaseComponent implements OnInit {
   @Input() pagina: string;
+  showCentros =true;
   ngOnInit() {
     let usuario = this.rest.getUsuarioSesion();
   }
 
   isNavbarCollapsed=false;
+  logout() {
+    // remove user from local storage and set current user to null
+    localStorage.clear();
+    // localStorage.removeItem('usuario');
+    // localStorage.removeItem('session_token');
+    // localStorage.removeItem('id_organizacion');
+    this.rest.currentUserSubject.next(null);
+    this.router.navigate(['/login']);
+  }
 }
