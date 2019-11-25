dac8f923da8059ab8235782b1af6b1bb66110662
nextor <nextor@gmail.com>
2019-11-25 09:03:28 -0800

    Refactorizacion espacios

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,44 +5,42 @@ import { Router } from '@angular/router';
 import { Usuario } from 'src/app/models/Modelos';
 import { ThrowStmt } from '@angular/compiler';
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.css"]
+	selector: "app-header",
+	templateUrl: "./header.component.html",
+	styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent extends BaseComponent implements OnInit {
-  @Input() pagina: string;
-  showCentros =false;
-  usuario=null
-  ngOnInit() {
-    let usuario = this.rest.getUsuarioSesion();
-    if (usuario.tipo == "RECEPCIONISTA" || usuario.tipo == "ADMIN" || usuario.tipo == "ASISTENTE"){
-      this.rest.usuario.get(usuario.id).subscribe(params=>{
-        this.usuario=params
+	@Input() pagina: string;
+	showCentros =false;
+	usuario=null
+	ngOnInit() {
+		let usuario = this.rest.getUsuarioSesion();
+		if (usuario.tipo == "RECEPCIONISTA" || usuario.tipo == "ADMIN" || usuario.tipo == "ASISTENTE"){
+			this.rest.usuario.get(usuario.id).subscribe(params=>{
+				this.usuario=params
+			});
+		}
+		else if (usuario.tipo = "DOCTOR"){
+			this.rest.doctor.get(usuario.id).subscribe(params => {
+				this.usuario = params
 
-      });
-    }
-    else if (usuario.tipo = "DOCTOR"){
-      this.rest.doctor.get(usuario.id).subscribe(params => {
-        this.usuario = params
+			});
+		}
+		else if (usuario.tipo = "PACIENTE"){
+			this.rest.paciente.get(usuario.id).subscribe(params =>{
+				this.usuario= params
+			})
+		}
+	}
 
-    });
-  }
-    else if (usuario.tipo = "PACIENTE"){
-      this.rest.paciente.get(usuario.id).subscribe(params =>{
-        this.usuario= params
-
-      })
-    }
-  }
-
-  isNavbarCollapsed=false;
-  logout() {
-    // remove user from local storage and set current user to null
-    localStorage.clear();
-    // localStorage.removeItem('usuario');
-    // localStorage.removeItem('session_token');
-    // localStorage.removeItem('id_organizacion');
-    this.rest.currentUserSubject.next(null);
-    this.router.navigate(['/login']);
-  }
+	isNavbarCollapsed=false;
+	logout() {
+		// remove user from local storage and set current user to null
+		localStorage.clear();
+		// localStorage.removeItem('usuario');
+		// localStorage.removeItem('session_token');
+		// localStorage.removeItem('id_organizacion');
+		this.rest.currentUserSubject.next(null);
+		this.router.navigate(['/login']);
+	}
 }
