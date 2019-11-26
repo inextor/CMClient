a076c63302ac4d42d18d42bb85cec9e85e796f3a
Dhantox <dhantox2@hotmail.com>
2019-11-26 15:05:50 -0800

    agregando modal a logout

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -11,15 +11,19 @@ export class MenuComponent implements OnInit {
   constructor(private rest: RestService, public router: Router ) {}
   showCentros:boolean = false;
   show_logout: boolean = false;
+
   ngOnInit() {}
 
   showLogoutModal() {
     this.show_logout=true;
   }
 
+  
+
   logout() {
     // remove user from local storage and set current user to null
     localStorage.clear();
+    this.show_logout=false
     // localStorage.removeItem('usuario');
     // localStorage.removeItem('session_token');
     // localStorage.removeItem('id_organizacion');
