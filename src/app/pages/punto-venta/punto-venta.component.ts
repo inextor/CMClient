865df6ae37d0bdb0f2ffebdd817cf9573b1ff304
nextor <nextor@gmail.com>
2019-11-28 09:30:58 -0800

    Agregando punto de venta

---
+++ src/app/pages/punto-venta/punto-venta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-punto-venta',
+  templateUrl: './punto-venta.component.html',
+  styleUrls: ['./punto-venta.component.css']
+})
+export class PuntoVentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
