ad01f0f5675c6a957908c13fb3d5fcd432553e3f
nextor <nextor@gmail.com>
2019-11-29 13:14:52 -0800

    Los modales se cierran con esc

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter  } from '@angular/core';
+import { RestService } from '../../services/rest.service';
+import { Observable, BehaviorSubject,forkJoin } from 'rxjs';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+	selector: 'app-modal',
+	templateUrl: './modal.component.html',
+	styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
 
-  @Input() show:boolean = false;
+	@Input() show:boolean = false;
+	@Input() closable:boolean = true;
+	@Output() showChange= new EventEmitter<boolean>();
 
-  constructor() { }
+	constructor(private rest:RestService) {
 
-  ngOnInit() {
-  }
+	}
 
+	ngOnInit() {
+		this.rest.keyUpObserver.subscribe((e)=>
+		{
+			if( e.keyCode == 27 )
+			{
+				if( this.closable )
+				{
+					this.showChange.emit( false );
+				}
+			}
+		});
+	}
 }
