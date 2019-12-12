785198bbc93bf2f937c54b87cc501cd371676756
nextor <nextor@gmail.com>
2019-12-12 08:18:46 -0800

    Refactor a ticket venta

--- src/app/components/ticket-venta/ticket-venta.component.ts
+++ src/app/components/ticket-venta/ticket-venta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,Output,EventEmitter  } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter	} from '@angular/core';
 import { RestService } from '../../services/rest.service';
 import { Observable, BehaviorSubject,forkJoin } from 'rxjs';
 import { Requisicion, Venta, Usuario, Pago, Detalle_Venta, Centro_Medico } from 'src/app/models/Modelos';
@@ -6,73 +6,32 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from	'@angular/common';
 import { Title } from '@angular/platform-browser';
 import { BaseComponent } from 'src/app/pages/base/base.component';
-import { formatDate } from '@fullcalendar/core';
+import { DatosVenta } from '../../services/rest.service';
 
 @Component({
-  selector: 'app-ticket-venta',
-  templateUrl: './ticket-venta.component.html',
-  styleUrls: ['./ticket-venta.component.css']
+	selector: 'app-ticket-venta',
+	templateUrl: './ticket-venta.component.html',
+	styleUrls: ['./ticket-venta.component.css']
 })
 
 export class TicketVentaComponent extends BaseComponent implements OnInit {
-	@Input() show:boolean = false;
-	@Input() closable:boolean = true;
-  @Output() showChange= new EventEmitter<boolean>();
-  constructor( public rest:RestService, public router:Router, public route:ActivatedRoute, public location: Location, public titleService:Title)
+
+	_datosVenta:DatosVenta
+
+	@Input('datosVenta')
+	set datosVenta(dv:DatosVenta)
 	{
-    super( rest,router,route,location,titleService);
-  }
+		console.log('Detalles Venta', dv );
+		this._datosVenta = dv;
+		this.indice_pago = 0;
+	}
 
-  busqueda: string = '';
-  venta: Venta=null;
-  vent={
-    id:null
-  };
-  usuario=null;
-  detalle_ventas: Detalle_Venta[]=[];
-  servicio=null;
-  centro_medico=null;
-  todayDate : Date = new Date();
-  ngOnInit() {
-    let centro_medico=this.rest.getCurrentCentroMedico();
-      this.rest.centro_medico.search({ eq: { id: centro_medico.id} }).subscribe((respuestas) => {
-      this.centro_medico = respuestas;
-      console.log(this.centro_medico)
-    }, (error) => this.showError(error));
+	@Input() indice_pago:number = 0;
 
-    this.rest.keyUpObserver.subscribe((e)=>
-		{
-			if( e.keyCode == 27 )
-			{
-				if( this.closable )
-				{
-					this.showChange.emit( false );
-				}
-			}
-		});
-  }
+	ngOnInit()
+	{
 
-  buscar(){
-
-    forkJoin([
-      this.rest.venta.search({ eq: { id: this.vent.id} }),
-    ]).subscribe((respuestas) => {
-      this.venta = respuestas[0];
-      forkJoin([
-        this.rest.usuario.search({eq:{id:this.venta.id_usuario_atendio}}),
-        this.rest.detalle_venta.search({eq:{id_venta:this.venta.id}}),
-      ]).subscribe((respuestas)=>{
-        this.usuario = respuestas[0];
-        this.detalle_ventas = respuestas[1].datos;
-
-        this.rest.servicio.search({eq:{id:this.detalle_ventas[0].id_servicio}}).subscribe((respuestas)=>{
-          this.servicio = respuestas;
-
-
-        }  ),(error)=>this.showError(error);
-      },(error) => this.showError(error));
-    }, (error) => this.showError(error))
-    //no corresponde con el id recepcionista con los usuarios
-  }
+	}
 
 }
+
