7a51f2fbef9610bcb176a253c990290ead98aea2
nextor <nextor@gmail.com>
2019-12-16 08:43:10 -0800

    Removing spaces

--- src/app/pages/ingresos/ingresos.component.ts
+++ src/app/pages/ingresos/ingresos.component.ts
@@ -8,31 +8,30 @@ import { SearchGastoCentroMedicoResponse } from '../../models/Respuestas';
 import { Location } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 @Component({
-  selector: 'app-ingresos',
-  templateUrl: './ingresos.component.html',
-  styleUrls: ['./ingresos.component.css']
+	selector: 'app-ingresos',
+	templateUrl: './ingresos.component.html',
+	styleUrls: ['./ingresos.component.css']
 })
 export class IngresosComponent extends BaseComponent implements OnInit {
-  showAddIngreso: boolean = false;
-  ingresos: Ingreso[] = [];
+	showAddIngreso: boolean = false;
+	ingresos: Ingreso[] = [];
 
 	constructor( public rest:RestService, public router:Router, public route:ActivatedRoute, public location: Location, public titleService:Title)
 	{
 		super( rest,router,route,location,titleService);
 	}
-  ngOnInit() {
-    this.titleService.setTitle('Ingresos');
-    let usuario = this.rest.getUsuarioSesion().id;
-    console.log(usuario)
-    this.is_loading = true;
+	ngOnInit() {
+		this.titleService.setTitle('Ingresos');
+		let usuario = this.rest.getUsuarioSesion().id;
+		console.log(usuario)
+		this.is_loading = true;
 
-      this.rest.ingreso.getAll({})
-        //this.rest.getDoctores()
-        .subscribe(respuesta => {
-          this.is_loading = false;
-          this.ingresos = respuesta.datos;
-          console.log(this.ingresos);
-        })
-        }
+		this.rest.ingreso.getAll({})
+			.subscribe(respuesta => {
+				this.is_loading = false;
+				this.ingresos = respuesta.datos;
+				console.log(this.ingresos);
+			})
+	}
 
 }
