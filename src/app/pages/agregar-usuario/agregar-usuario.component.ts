be0ebdc4a77cb8e84028faa35b3c8e9126f491cf
Dhantox <a338750@uabc.edu.mx>
2020-01-23 13:28:18 -0800

    actualizando imagen de usuario y webservice post usuario

--- src/app/pages/agregar-usuario/agregar-usuario.component.ts
+++ src/app/pages/agregar-usuario/agregar-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { RestService } from '../../services/rest.service';
 import { Usuario , Paciente } from '../../models/Modelos';
 import {Router,ActivatedRoute} from "@angular/router"
@@ -17,10 +17,8 @@ export class AgregarUsuarioComponent extends BaseComponent implements OnInit {
 		id_organizacion: null,
 		contrasena: '',
 		correo_electronico:'',
-		tipo: null
+		tipo: null,
 	};
-
-
 	confirmar_contrasena:string = '';
 
 	ngOnInit()
@@ -28,6 +26,19 @@ export class AgregarUsuarioComponent extends BaseComponent implements OnInit {
 		this.route.paramMap.subscribe( params =>{
 			let currentUser = this.rest.getUsuarioSesion();
 			this.usuario.id_organizacion = currentUser.id_organizacion;
+
+			this.usuario.id			= params.get('id') ==null ? null : parseInt(params.get('id') );;
+
+			if( this.usuario.id )
+			{
+				//forkJoin([
+				//])
+				this.rest.usuario.get( this.usuario.id )
+				.subscribe((responses)=>
+				{
+					this.usuario= responses;
+				});
+			}
 		});
 	}
 
@@ -40,12 +51,43 @@ export class AgregarUsuarioComponent extends BaseComponent implements OnInit {
 			return;
 		}
 
-		this.is_loading = true;
+		// this.is_loading = true;
 
-		this.rest.agregarUsuario( this.usuario ).subscribe((usuario)=>
+		// this.rest.agregarUsuario( this.usuario ).subscribe((usuario)=>
+		// {
+		// 	this.is_loading = false;
+		// 	this.router.navigate(['/usuarios']);
+		// }, error=> this.showError(error) );
+
+		// this.is_loading = true;
+
+		if( this.usuario.id	)
 		{
-			this.is_loading = false;
-			this.router.navigate(['/usuarios']);
-		}, error=> this.showError(error) );
+			this.rest.usuario.update( this.usuario ).subscribe((usuario)=>{
+				this.is_loading = false;
+				this.router.navigate(['/usuarios']);
+			},(error)=>this.showError(error));
+		}
+		else
+		{
+			console.log("test",this.usuario);
+			this.rest.usuario.create( this.usuario ).subscribe((usuario)=>{
+				this.is_loading = false;
+			
+				this.router.navigate(['/usuarios']);
+			},(error)=>this.showError(error));
+		}
 	}
+
+	uploadImage(evt)
+	{
+		if (evt.target.files.length)
+		{
+			this.rest.uploadImage(evt.target.files[0], false).subscribe((imageData) => {
+				this.usuario.id_imagen = imageData.id;
+			}, error => this.showError(error));
+		}
+	}
+
+
 }
